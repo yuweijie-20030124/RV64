9a8f4002cad6cc2ac6829c31e9e6b1f2e02d14a5
tracer-ysyx <tracer@ysyx.org>
2026-04-26 13:47:00 +0800

    > compile NEMU ysyx_22040000 张三 Linux Yihan 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:47:00 up 101 days, 21:02,  9 users,  load average: 0.08, 0.08, 0.05

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -26,7 +26,7 @@ const char *regs[] = {
 void isa_reg_display() {
     printf("PC\t0x%016lx\t%ld\n", cpu.pc, cpu.pc);
     for (int i = 0; i < 32; i++) {
-        printf("%s\t0x%016lx\t%ld\t", reg_name(i), gpr(i), gpr(i));
+        printf("%s  0x%016lx\t%ld\t", reg_name(i), gpr(i), gpr(i));
         if ((i + 1) % 4 == 0) {
             printf("\n");   // 每四个换行
         } else {
