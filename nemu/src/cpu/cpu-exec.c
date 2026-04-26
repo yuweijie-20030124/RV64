ed9492b859f20a70e424470df0ba9b3cd5037503
tracer-ysyx <tracer@ysyx.org>
2026-04-26 22:21:13 +0800

    > compile NEMU ysyx_22040000 张三 Linux Yihan 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:21:13 up 102 days,  5:36, 11 users,  load average: 0.03, 0.01, 0.04

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -73,6 +73,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst, ilen);
+    enqueue(&cb, s->logbuf);
 #endif
 }
 
