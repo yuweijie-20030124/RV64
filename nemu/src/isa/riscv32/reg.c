8cd2d294f3ebeeaa08e2f20a0635c0bb0348fd27
tracer-ysyx <tracer@ysyx.org>
2026-04-26 21:40:35 +0800

    > compile NEMU ysyx_22040000 张三 Linux Yihan 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:40:35 up 102 days,  4:56, 11 users,  load average: 0.15, 0.08, 0.03

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -24,27 +24,15 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
-    // 打印 PC，宽度与寄存器行保持一致
-    printf("%-8s 0x%016lx %12ld\n", "PC", cpu.pc, cpu.pc);
-
+    printf("PC\t0x%016lx\t%ld\n", cpu.pc, cpu.pc);
     for (int i = 0; i < 32; i++) {
-        // 每个寄存器占用固定宽度：名称8字符、十六进制16字符、十进制12字符
-        printf("%-8s 0x%016lx %12ld", reg_name(i), gpr(i), gpr(i));
-        
-        // 每打印4个寄存器换行，否则输出一个空格分隔（不用制表符）
+        printf("%s\t0x%016lx\t%ld", reg_name(i), gpr(i), gpr(i));
         if ((i + 1) % 4 == 0) {
-            printf("\n");
+            printf("\n");   // 每四个换行
         } else {
-            printf("  ");   // 两个空格分隔不同寄存器块
+            printf("\t");   // 否则输出制表符作为分隔
         }
     }
-
-    //打印csr寄存器
-    printf("%-8s 0x%016lx %12ld\n", "mcause"  , cpu.mcause  , cpu.mcause  );
-    printf("%-8s 0x%016lx %12ld\n", "mstatus" , cpu.mstatus , cpu.mstatus );
-    printf("%-8s 0x%016lx %12ld\n", "mepc"    , cpu.mepc    , cpu.mepc    );
-    printf("%-8s 0x%016lx %12ld\n", "mtvec"   , cpu.mtvec   , cpu.mtvec   );
-    printf("\n");
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
