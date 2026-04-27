5dcbb4b527991063b12fa8af0547e348233c539e
tracer-ysyx <tracer@ysyx.org>
2026-04-27 13:19:01 +0800

    > compile NEMU ysyx_22040000 张三 Linux Yihan 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:19:01 up 102 days, 20:34, 10 users,  load average: 0.04, 0.01, 0.00

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,6 +1,6 @@
 #include <proc.h>
 #include <elf.h>
-
+#include <fs.h>
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
 # define Elf_Phdr Elf64_Phdr
@@ -9,9 +9,43 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+#if defined(__ISA_RISCV32__) || defined(__ISA_RISCV64__)
+# define EXPECT_TYPE EM_RISCV
+#elif defined(__ISA_AM_NATIVE__) || defined(__ISA_X86_64__)
+# define EXPECT_TYPE EM_X86_64
+#else
+# error Unsupported ISA
+#endif
+
+#define USER_HEAP_GAP (8 * 1024 * 1024)
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  int fd = fs_open(filename, 0, 0);
+  uint32_t phdr_size = sizeof(Elf_Phdr);
+  Elf_Ehdr ehdr;
+  Elf_Phdr phdr;
+  uintptr_t max_brk = 0;
+  fs_read(fd, &ehdr, sizeof(Elf_Ehdr));
+  assert(*(uint32_t *)ehdr.e_ident == 0x464c457f);
+  assert(EXPECT_TYPE == ehdr.e_machine);
+  uint32_t phdr_num = ehdr.e_phnum;
+  for (int i = 0; i < phdr_num; i++) {
+    fs_lseek(fd, ehdr.e_phoff + i * phdr_size, SEEK_SET);
+    fs_read(fd, &phdr, phdr_size);
+    if (phdr.p_type != PT_LOAD) continue;
+    fs_lseek(fd, phdr.p_offset, SEEK_SET);
+    fs_read(fd, (void *)phdr.p_vaddr, phdr.p_filesz);
+    memset((void *)phdr.p_vaddr + phdr.p_filesz, 0, phdr.p_memsz - phdr.p_filesz);
+    uintptr_t brk = phdr.p_vaddr + phdr.p_memsz;
+    if (max_brk < brk) {
+      max_brk = brk;
+    }
+  }
+  fs_close(fd);
+  if (pcb != NULL) {
+    pcb->max_brk = ROUNDUP(max_brk, PGSIZE);
+  }
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
