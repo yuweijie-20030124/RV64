9bd1e49b6cc06f140949663719b5e630351762bb
tracer-ysyx <tracer@ysyx.org>
2026-05-08 17:00:20 +0800

    > compile NEMU ysyx_22040000 张三 Linux Enine 6.17.0-22-generic #22~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Mar 26 15:25:54 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:00:20 up 2 days, 25 min,  4 users,  load average: 3.02, 3.01, 3.00

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,8 +1,55 @@
 #include <common.h>
 #include "syscall.h"
+
+#define CONFIG_STRACE 0
+
+#if CONFIG_STRACE
+#define STRACE_LOG(...) Log(__VA_ARGS__)
+#else
+#define STRACE_LOG(...)
+#endif
+
+#if CONFIG_STRACE
+#define STRACE_FD(fd) fs_fd_name(fd)
+#else
+#define STRACE_FD(fd) NULL
+#endif
+
+static const char *syscall_names[] = {
+  [SYS_exit] = "exit",
+  [SYS_yield] = "yield",
+  [SYS_open] = "open",
+  [SYS_read] = "read",
+  [SYS_write] = "write",
+  [SYS_kill] = "kill",
+  [SYS_getpid] = "getpid",
+  [SYS_close] = "close",
+  [SYS_lseek] = "lseek",
+  [SYS_brk] = "brk",
+  [SYS_fstat] = "fstat",
+  [SYS_time] = "time",
+  [SYS_signal] = "signal",
+  [SYS_execve] = "execve",
+  [SYS_fork] = "fork",
+  [SYS_link] = "link",
+  [SYS_unlink] = "unlink",
+  [SYS_wait] = "wait",
+  [SYS_times] = "times",
+  [SYS_gettimeofday] = "gettimeofday",
+};
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
+  const char *name = "unknown";
+
+  if (a[0] < LENGTH(syscall_names) && syscall_names[a[0]] != NULL) {
+    name = syscall_names[a[0]];
+  }
+  (void)name;
 
   switch (a[0]) {
     default: panic("Unhandled syscall ID = %d", a[0]);
