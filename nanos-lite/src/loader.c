7c8781cad221cdda6fe798aa56c336ef4744ede9
tracer-ysyx <tracer@ysyx.org>
2026-05-09 13:34:27 +0800

    > compile NEMU ysyx_22040000 张三 Linux Enine 6.17.0-22-generic #22~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Mar 26 15:25:54 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:34:27 up 2 days, 20:59,  6 users,  load average: 1.02, 1.05, 1.02

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -27,7 +27,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   uintptr_t max_brk = 0;
   fs_read(fd, &ehdr, sizeof(Elf_Ehdr));
   // 可以在loader中对魔数进行检查:
-  assert(*(uint32_t *)ehdr.e_ident == 0x164c457f);
+  assert(*(uint32_t *)ehdr.e_ident == 0x464c457f);
   assert(EXPECT_TYPE == ehdr.e_machine);
   uint32_t phdr_num = ehdr.e_phnum;
   for (int i = 0; i < phdr_num; i++) {
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -6,6 +6,9 @@ static PCB pcb[MAX_NR_PROC] __attribute__((used)) = {};
 static PCB pcb_boot = {};
 PCB *current = NULL;
 
+void naive_uload();
+
+
 void switch_boot_pcb() {
   current = &pcb_boot;
 }
@@ -25,7 +28,7 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
-
+  naive_uload();
 }
 
 Context* schedule(Context *prev) {
