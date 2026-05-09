e36c159c8eb771d8a0d3af2ee9adae64d6d0191e
tracer-ysyx <tracer@ysyx.org>
2026-05-09 14:20:41 +0800

    > compile NEMU ysyx_22040000 张三 Linux Enine 6.17.0-22-generic #22~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Mar 26 15:25:54 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:20:41 up 2 days, 21:45,  6 users,  load average: 1.08, 1.02, 1.01

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -57,5 +57,7 @@ void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %p", entry);
   ((void(*)())entry) ();
+  Log("Jump out of entry = %p", entry);
+
 }
 
