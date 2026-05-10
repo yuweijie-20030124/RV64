a2c39fc3d6477b2563f2486c69e4e148a509529e
tracer-ysyx <tracer@ysyx.org>
2026-05-10 13:14:05 +0800

    > compile NEMU ysyx_22040000 张三 Linux Enine 6.17.0-22-generic #22~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Mar 26 15:25:54 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:14:05 up 3 days, 20:39,  6 users,  load average: 1.06, 1.03, 1.00

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -29,7 +29,8 @@ static const uint32_t img [] = {
 static void restart() {
   /* Set the initial program counter. */
   cpu.pc = RESET_VECTOR;
-  cpu.mstatus = 0xa00021800;
+  cpu.mstatus = 0xa00001800;
+  // cpu.mstatus = 0xa00021800;
   /* The zero register is always 0. */
   cpu.gpr[0] = 0;
 }
