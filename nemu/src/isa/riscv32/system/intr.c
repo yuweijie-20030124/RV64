d0d8ea2d34589990e83c044aa36467c614b526f0
tracer-ysyx <tracer@ysyx.org>
2026-05-10 19:07:19 +0800

    > compile NEMU ysyx_22040000 张三 Linux Enine 6.17.0-22-generic #22~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Mar 26 15:25:54 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:07:19 up 4 days,  2:32,  4 users,  load average: 1.08, 1.02, 1.01

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -20,7 +20,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    * Then return the address of the interrupt/exception vector. 然后返回中断/异常向量的地址
    */
   // cpu.mstatus = 0xa00001800; 
-  cpu.mstatus = 0xa00001800; 
+  cpu.mstatus = 0xa00021800; 
   cpu.mepc = epc; 
   cpu.mcause = NO;
   return cpu.mtvec;
