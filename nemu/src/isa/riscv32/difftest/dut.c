fbf0543fa4caa8f959e6817f20dd671ac38a02e1
tracer-ysyx <tracer@ysyx.org>
2026-05-10 19:05:37 +0800

    > compile NEMU ysyx_22040000 张三 Linux Enine 6.17.0-22-generic #22~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Mar 26 15:25:54 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:05:37 up 4 days,  2:30,  4 users,  load average: 1.04, 1.01, 1.00

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -32,12 +32,12 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
     }
   }
 
-  if (ref_r->mtvec != cpu.mtvec) {
-    printf("csr reg mtvec is wrong\n");
-    printf("wrong reg value is 0x%08lx\n",cpu.mtvec);
-    printf("right reg value is 0x%08lx\n",ref_r->mtvec);
-    return false;
-  }
+  // if (ref_r->mtvec != cpu.mtvec) {
+  //   printf("csr reg mtvec is wrong\n");
+  //   printf("wrong reg value is 0x%08lx\n",cpu.mtvec);
+  //   printf("right reg value is 0x%08lx\n",ref_r->mtvec);
+  //   return false;
+  // }
   // if (ref_r->mepc != cpu.mepc) {
   //   printf("csr reg mepc is wrong\n");
   //   printf("wrong reg value is 0x%08lx\n",cpu.mepc);
