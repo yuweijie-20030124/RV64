ea00954394944674969f9cd61763d0d8c942b530
tracer-ysyx <tracer@ysyx.org>
2026-06-01 11:08:23 +0800

    > compile NEMU ysyx_22040000 张三 Linux Enine 6.17.0-22-generic #22~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Mar 26 15:25:54 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:08:23 up 25 days, 18:33, 18 users,  load average: 2.31, 2.34, 2.62

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -21,6 +21,41 @@
 #include <cpu/decode.h>
 #include "isa-def.h"
 
+typedef enum {
+  DIFFTEST_PRV_U = 0,
+  DIFFTEST_PRV_S = 1,
+  DIFFTEST_PRV_M = 3,
+} DifftestPrivileged;
+
+typedef struct {
+  word_t gpr[32];
+  word_t pc;
+  DifftestPrivileged privilege;
+  word_t mtvec, mstatus, mcause, mepc, mtval;
+  word_t stvec, sepc, scause, stval;
+  word_t medeleg, mideleg;
+  word_t mip, mie;
+  word_t mcycle, minstret, mhpmcounter[29], mhpmevent[29];
+  uint32_t mcountinhibit;
+  word_t mscratch;
+  word_t sscratch;
+  word_t satp;
+#ifndef CONFIG_RV64
+  word_t mcycleh, minstreth, mhpmcounterh[29], mhpmeventh[29];
+#endif
+  word_t misa, menvcfg, mseccfg;
+  word_t senvcfg;
+  uint32_t mcounteren;
+  uint32_t scounteren;
+#ifndef CONFIG_RV64
+  word_t mstatush;
+  word_t menvcfgh, mseccfgh;
+#endif
+  word_t mvendorid, marchid, mimpid, mhartid, mconfigptr;
+} DifftestCPUState;
+
+static DifftestCPUState ref_shadow;
+
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
     if (direction == DIFFTEST_TO_REF){
         memcpy(guest_to_host(addr), buf, n);
@@ -29,60 +64,35 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
 
 
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
-    CPU_state *dut_regs;
-    dut_regs = (CPU_state *)dut;
+    DifftestCPUState *dut_regs = (DifftestCPUState *)dut;
     if (direction == DIFFTEST_TO_REF){
+        memcpy(&ref_shadow, dut_regs, sizeof(ref_shadow));
         for (int i = 0; i < 32;i++){
             cpu.gpr[i] = dut_regs->gpr[i];
         }
         cpu.pc = dut_regs->pc;
-        // cpu.privilege = dut_regs->privilege;
-        // cpu.mepc = dut_regs->mepc;
-        // cpu.mtvec = dut_regs->mtvec;
-        // cpu.mstatus = dut_regs->mstatus;
-        // cpu.mcause = dut_regs->mcause;
-        // cpu.mtval = dut_regs->mtval;
-        // cpu.mscratch = dut_regs->mscratch;
-        // cpu.mideleg = dut_regs->mideleg;
-        // cpu.medeleg = dut_regs->medeleg;
-        // cpu.mip = dut_regs->mip;
-        // cpu.mie = dut_regs->mie;
-        // cpu.sepc = dut_regs->sepc;
-        // cpu.stvec = dut_regs->stvec;
-        // cpu.scause = dut_regs->scause;
-        // cpu.stval = dut_regs->stval;
-        // cpu.sscratch = dut_regs->sscratch;
-        // cpu.satp = dut_regs->satp;
+        cpu.mtvec = dut_regs->mtvec;
+        cpu.mstatus = dut_regs->mstatus;
+        cpu.mcause = dut_regs->mcause;
+        cpu.mepc = dut_regs->mepc;
     }
     else if (direction == DIFFTEST_TO_DUT)
     {
+        memcpy(dut_regs, &ref_shadow, sizeof(ref_shadow));
         for (int i = 0; i < 32; i++)
         {
             dut_regs->gpr[i] = cpu.gpr[i];
         }
         dut_regs->pc = cpu.pc;
-        // dut_regs->privilege = cpu.privilege;
-        // dut_regs->mepc = cpu.mepc;
-        // dut_regs->mtvec = cpu.mtvec;
-        // dut_regs->mstatus = cpu.mstatus;
-        // dut_regs->mcause = cpu.mcause;
-        // dut_regs->mtval = cpu.mtval;
-        // dut_regs->mscratch = cpu.mscratch;
-        // dut_regs->mideleg = cpu.mideleg;
-        // dut_regs->medeleg = cpu.medeleg;
-        // dut_regs->mip = cpu.mip;
-        // dut_regs->mie = cpu.mie;
-        // dut_regs->sepc = cpu.sepc;
-        // dut_regs->stvec = cpu.stvec;
-        // dut_regs->scause = cpu.scause;
-        // dut_regs->stval = cpu.stval;
-        // dut_regs->sscratch = cpu.sscratch;
-        // dut_regs->satp = cpu.satp;
+        dut_regs->privilege = DIFFTEST_PRV_M;
+        dut_regs->mtvec = cpu.mtvec;
+        dut_regs->mstatus = cpu.mstatus;
+        dut_regs->mcause = cpu.mcause;
+        dut_regs->mepc = cpu.mepc;
     }
     else{
         assert(0);
     }
-    //   assert(0);
 }
 
 __EXPORT void difftest_exec(uint64_t n) {
