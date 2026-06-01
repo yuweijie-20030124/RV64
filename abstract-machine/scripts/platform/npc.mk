94170eb871dfc206bb31efbc401f836ea249d3f0
tracer-ysyx <tracer@ysyx.org>
2026-06-01 10:47:21 +0800

    > compile NEMU ysyx_22040000 张三 Linux Enine 6.17.0-22-generic #22~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Mar 26 15:25:54 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:47:21 up 25 days, 18:12, 18 users,  load average: 2.21, 2.35, 3.44

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -8,12 +8,15 @@ AM_SRCS := riscv/npc/start.S \
            platform/dummy/vme.c \
            platform/dummy/mpe.c
 
-CFLAGS    += -fdata-sections -ffunction-sections -mstrict-align -I$(AM_HOME)/am/src/riscv/npc/include
+NPC_HOME ?= $(abspath $(AM_HOME)/../npc)
+
+CFLAGS    += -g -fdata-sections -ffunction-sections
 LDSCRIPTS += $(AM_HOME)/scripts/linker.ld
 LDFLAGS   += --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 NPCFLAGS  += -l $(shell dirname $(IMAGE).elf)/npc-log.txt
 NPCFLAGS  += -b
+NPCFLAGS  += -d --diff=$(RV64_NEMU_HOME)/build/riscv32-nemu-interpreter-so
 
 MAINARGS_MAX_LEN = 64
 MAINARGS_PLACEHOLDER = the_insert-arg_rule_in_Makefile_will_insert_mainargs_here
@@ -26,6 +29,12 @@ image: image-dep
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
+
+check-npc-home:
+	@if [ ! -f "$(NPC_HOME)/Makefile" ]; then \
+		echo "Error: NPC project not found under $(NPC_HOME)"; \
+		exit 1; \
+	fi
 
 run: insert-arg check-npc-home
 	$(MAKE) -C $(NPC_HOME) run NPC_IMG=$(IMAGE).bin NPC_ELF=$(IMAGE).elf NPC_FLAGS="$(NPCFLAGS)"
