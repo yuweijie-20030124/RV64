12fd595396ea1932f06c5bd4f8d94cb26d98abea
tracer-ysyx <tracer@ysyx.org>
2026-06-02 17:28:00 +0800

    > compile NEMU ysyx_22040000 张三 Linux ywj 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  17:28:00 up  1:36,  2 users,  load average: 0.13, 0.07, 0.08

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -41,9 +41,7 @@ void device_update() {
   }
   last = now;
 
-  IFDEF(CONFIG_HAS_VGA, 
-    printf("caosinide\n");
-    vga_update_screen());
+  IFDEF(CONFIG_HAS_VGA,vga_update_screen());
 
 #ifndef CONFIG_TARGET_AM
   SDL_Event event;
@@ -81,7 +79,7 @@ void init_device() {
 
   IFDEF(CONFIG_HAS_SERIAL, init_serial());
   IFDEF(CONFIG_HAS_TIMER, init_timer());
-  IFDEF(CONFIG_HAS_VGA, init_vga());
+  IFDEF(CONFIG_HAS_VGA,     printf("caosinide\n");init_vga());
   IFDEF(CONFIG_HAS_KEYBOARD, init_i8042());
   IFDEF(CONFIG_HAS_AUDIO, init_audio());
   IFDEF(CONFIG_HAS_DISK, init_disk());
