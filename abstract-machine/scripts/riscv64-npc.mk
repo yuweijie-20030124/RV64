a6ada2e168efde64f93ed0c0ce5b371266385442
tracer-ysyx <tracer@ysyx.org>
2026-06-02 13:06:09 +0800

    > compile NEMU ysyx_22040000 张三 Linux ywj 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  13:06:09 up 13 min,  1 user,  load average: 0.69, 0.83, 0.50

--- abstract-machine/scripts/riscv64-npc.mk
+++ abstract-machine/scripts/riscv64-npc.mk
@@ -1,8 +1,9 @@
 include $(AM_HOME)/scripts/isa/riscv.mk
 include $(AM_HOME)/scripts/platform/npc.mk
 
-CROSS_COMPILE := riscv64-unknown-elf-
+# CROSS_COMPILE := riscv64-unknown-elf-
 COMMON_CFLAGS := -fno-pic -march=rv64ima_zicsr -mabi=lp64 -mcmodel=medany -mstrict-align
+# COMMON_CFLAGS := -fno-pic -march=rv64im -mabi=lp64 -mcmodel=medany -mstrict-align
 
 CFLAGS  += $(COMMON_CFLAGS) -DISA_H=\"riscv/riscv.h\"
 ASFLAGS += $(COMMON_CFLAGS)
