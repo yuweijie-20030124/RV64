757823c3978cb6a11893fbc15735f9d2855f5f24
tracer-ysyx <tracer@ysyx.org>
2026-06-02 10:50:46 +0800

    > compile NEMU ysyx_22040000 张三 Linux ywj 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  10:50:46 up 38 min,  1 user,  load average: 0.39, 0.44, 0.33

---
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -0,0 +1,74 @@
+/***************************************************************************************
+* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
+*
+* NEMU is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
+
+#include <isa.h>
+#include <cpu/difftest.h>
+#include "../local-include/reg.h"
+
+extern const char *regs[];
+
+
+
+bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+  int reg_num = (int)(sizeof(cpu.gpr) / sizeof(cpu.gpr[0]));
+  if(ref_r->mcause != cpu.mcause){
+      printf("mcause is wrong\n");
+      return false;
+  }
+  for (int i = 0; i < reg_num; i++) {
+    if (ref_r->gpr[i] != cpu.gpr[i]) {
+      printf("reg %s is wrong\n",regs[i]);
+      printf("wrong reg value is 0x%08lx\n",cpu.gpr[i]);
+      printf("right reg value is 0x%08lx\n",ref_r->gpr[i]);
+      return false;
+    }
+  }
+
+  // if (ref_r->mtvec != cpu.mtvec) {
+  //   printf("csr reg mtvec is wrong\n");
+  //   printf("wrong reg value is 0x%08lx\n",cpu.mtvec);
+  //   printf("right reg value is 0x%08lx\n",ref_r->mtvec);
+  //   return false;
+  // }
+  // if (ref_r->mepc != cpu.mepc) {
+  //   printf("csr reg mepc is wrong\n");
+  //   printf("wrong reg value is 0x%08lx\n",cpu.mepc);
+  //   printf("right reg value is 0x%08lx\n",ref_r->mepc);
+  //   return false;
+  // }
+  // if (ref_r->mcause != cpu.mcause) {
+  //   printf("csr reg mcause is wrong\n");
+  //   printf("wrong reg value is 0x%08lx\n",cpu.mcause);
+  //   printf("right reg value is 0x%08lx\n",ref_r->mcause);
+  //   return false;
+  // }
+  // if (ref_r->mstatus != cpu.mstatus) {
+  //   printf("csr reg mstatus is wrong\n");
+  //   printf("wrong reg value is 0x%08lx\n",cpu.mstatus);
+  //   printf("right reg value is 0x%08lx\n",ref_r->mstatus);
+  //   return false;
+  // }
+
+  if (ref_r->pc != cpu.pc) {
+    printf("pc is wrong\n");
+    printf("wrong pc is 0x%08lx\n",cpu.pc);
+    printf("right pc is 0x%08lx\n",ref_r->pc);
+    return false;
+  }
+  return true;
+}
+
+void isa_difftest_attach() {
+}
---
+++ nemu/src/isa/riscv64/include/isa-def.h
@@ -0,0 +1,39 @@
+/***************************************************************************************
+* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
+*
+* NEMU is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
+
+#ifndef __ISA_RISCV_H__
+#define __ISA_RISCV_H__
+
+#include <common.h>
+
+typedef struct {
+  word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)];
+  vaddr_t pc;
+  vaddr_t mcause;     // 存放触发异常的原因           0x342
+  vaddr_t mstatus;    // 存放处理器的状态             0x300
+  vaddr_t mepc;       // 存放触发异常的PC             0x341
+  vaddr_t mtvec;      // 指向trap处理函数的入口地址    0x305
+} MUXDEF(CONFIG_RV64, riscv64_CPU_state, riscv32_CPU_state);
+
+// decode
+typedef struct {
+  union {
+    uint32_t val;
+  } inst;
+} MUXDEF(CONFIG_RV64, riscv64_ISADecodeInfo, riscv32_ISADecodeInfo);
+
+#define isa_mmu_check(vaddr, len, type) (MMU_DIRECT)
+
+#endif
