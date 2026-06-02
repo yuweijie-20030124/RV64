df52876a71f1a1db6d04c2944575ef05f51b3577
tracer-ysyx <tracer@ysyx.org>
2026-06-02 15:46:08 +0800

    > compile NEMU ysyx_22040000 张三 Linux ywj 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  15:46:08 up  2:53,  1 user,  load average: 1.53, 1.38, 1.04

--- npc/include/config.h
+++ npc/include/config.h
@@ -5,11 +5,16 @@
 #define CONFIG_ISA64        1
 #define PMEM64              1
 
-// #define CONFIG_TRACE        1
-// #define CONFIG_ITRACE       1
+#define CONFIG_TRACE        1
+#define CONFIG_TRACE_START  0
+#define CONFIG_TRACE_END    10000
+#define CONFIG_ITRACE       1
 // #define CONFIG_WATCHPOINT   1
-// #define CONFIG_FTRACE       1
-// #define CONFIG_MTRACE       1
+#define CONFIG_FTRACE       1
+#define CONFIG_MTRACE       1
+#define CONFIG_MRACE_START  0
+#define CONFIG_MRACE_END    10000
+// #define CONFIG_DEVICE       1   // needs device_update() + sdl_clear_event_queue() implementations
 #define CONFIG_DIFFTEST     1
 // #define CONFIG_VCD_GET      1
 #define CONFIG_GET_TIMER    1
