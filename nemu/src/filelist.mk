799ff731210c209f9897e718bcba47bc1dc46f07
tracer-ysyx <tracer@ysyx.org>
2026-06-02 11:21:11 +0800

    > compile NEMU ysyx_22040000 张三 Linux ywj 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  11:21:11 up  1:08,  1 user,  load average: 1.73, 3.15, 3.23

--- nemu/src/filelist.mk
+++ nemu/src/filelist.mk
@@ -20,6 +20,7 @@ DIRS-BLACKLIST-$(CONFIG_TARGET_AM) += src/monitor/sdb
 
 SHARE = $(if $(CONFIG_TARGET_SHARE),1,0)
 LIBS += $(if $(CONFIG_TARGET_NATIVE_ELF),-lreadline -ldl -pie,)
+LIBS += $(if $(CONFIG_TARGET_SHARE),-lreadline,)
 
 ifdef mainargs
 ASFLAGS += -DBIN_PATH=\"$(mainargs)\"
