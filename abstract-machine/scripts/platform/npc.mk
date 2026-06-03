150d7def3298ef3462294192b3720e01bafe84bb
tracer-ysyx <tracer@ysyx.org>
2026-06-03 16:32:05 +0800

    > compile NEMU ysyx_22040000 张三 Linux ywj 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  16:32:05 up 1 day, 40 min,  2 users,  load average: 0.10, 0.08, 0.08

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -16,7 +16,7 @@ LDSCRIPTS += $(AM_HOME)/scripts/linker.ld
 LDFLAGS   += --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 NPCFLAGS  += -l $(shell dirname $(IMAGE).elf)/npc-log.txt
-# NPCFLAGS  += -b
+NPCFLAGS  += -b
 NPCFLAGS  += -d --diff=$(NEMU_HOME)/build/riscv64-nemu-interpreter-so
 
 MAINARGS_MAX_LEN = 64
